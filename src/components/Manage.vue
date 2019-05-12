ee284fc534e6b3014bf9b8c43bae28fdfad66541
jianhui_kong <jianhui_kong@crfchina.com>
2019-05-12 10:13:39 +0800

    用户管理界面做好

--- src/components/Manage.vue
+++ src/components/Manage.vue
@@ -8,23 +8,26 @@
 	
 	
 
-<div>
-  
-
+<div class="d-flex justify-content-center">
+ 
     <b-modal
-      id="modal-prevent-closing"
+      id="modal-1"
       ref="modal"
-      title="Submit Your Name"
+      title="新增用户"
       @show="resetModal"
       @hidden="resetModal"
       @ok="handleOk"
+			
     >
-      <form ref="form" @submit.stop.prevent="handleSubmit">
+			<div class="d-flex  w-100 ">
+      <form ref="form" @submit.stop.prevent="onSubmit" >
         <b-form-group
+					
           :state="nameState"
-          label="Name"
+          label="登录账号"
           label-for="name-input"
           invalid-feedback="Name is required"
+					class="text-left w-100 "
         >
           <b-form-input
             id="name-input"
@@ -32,9 +35,74 @@
             :state="nameState"
             required
           ></b-form-input>
-        </b-form-group>
-      </form>
+       
+			  <b-form-invalid-feedback :state="validation">
+        Your user ID must be 5-12 characters long.
+      </b-form-invalid-feedback>
+     
+			
+			
+				 </b-form-group>
+				
+      <b-form-group
+      	:state="nameState"
+      	label="登录密码"
+      	label-for="name-input"
+      	invalid-feedback="Name is required"
+					class="text-left"
+				
+      >
+      	<b-form-input
+      		id="name-input"
+      		v-model="name"
+      		:state="nameState"
+      		required
+      	></b-form-input>
+      </b-form-group>
+		
+			<b-form-group
+				:state="nameState"
+				label="确认密码"
+				label-for="name-input"
+				invalid-feedback="Name is required"
+					class=" text-left"
+				
+			>
+				<b-form-input
+					id="name-input"
+					v-model="name"
+					:state="nameState"
+					required
+				></b-form-input>
+			</b-form-group>
+			
+			<b-form-group
+				:state="nameState"
+				label="权限"
+				label-for="name-input"
+				invalid-feedback="Name is required"
+					class="text-left"
+				
+			>
+			
+			
+		  <b-form-checkbox-group
+        v-model="selected"
+        :options="options"
+        switches
+      ></b-form-checkbox-group>
+  	
+		
+			</b-form-group>
+			
+			
+
+			
+			
+			</form>
+			</div >
     </b-modal>
+		
   </div>
 	
 	
@@ -67,6 +135,9 @@
 				
 					<div class="d-flex justify-content-between">
 						<span><h6>上次登录时间:</h6></span><span>2018-12-12 12:12:12</span>
+					</div>
+					<div class="d-flex justify-content-between">
+						<span><h6>有效期至:</h6></span><span>2018-12-12</span>
 					</div>
 					<div class="d-flex justify-content-between">
 						<span><h6>用户状态:</h6></span><span>正常</span>
@@ -124,14 +195,13 @@
 	        align="center"
 					
 	      >
+				
 	      <b-card-text></b-card-text>
-					
-					<b-button variant="outline-primary">
+					<b-button v-b-modal.modal-1 variant="outline-primary">
 						<img  src="../assets/icon_addperson.png" alt="Image 1" width="30" height="30" class=""></img>
 					</b-button>
-						
-						
-	      </b-card>
+		
+				</b-card>
 	
 	      
 	    </b-card-group>
@@ -158,8 +228,22 @@
   export default {
     data() {
       return {
-       
+         selected: [], // Must be an array reference!
+        options: [
+          { text: '设备地图', value: 'orange' },
+          { text: '设备列表', value: 'apple' },
+          { text: '设备分析', value: 'pineapple' },
+          { text: '设备决策', value: 'grape' }
+        ],
       
+			 form: {
+          email: '',
+          name: '',
+          food: null,
+          checked: []
+        },
+        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true
       }
     },
     computed: {
@@ -169,7 +253,25 @@
      
     },
     methods: {
-      
+     
+			  onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+			
     }
   }
 </script>
