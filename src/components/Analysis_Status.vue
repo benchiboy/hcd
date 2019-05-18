8c7b14585576221050e614b21fa075a24d4fb6c3
jianhui_kong <jianhui_kong@crfchina.com>
2019-05-18 20:13:55 +0800

    完成ECHART MAP 的基础开发

--- src/components/Analysis_Status.vue
+++ src/components/Analysis_Status.vue
@@ -5,7 +5,7 @@
     
 		<b-button @click="makeData">MakeData</b-button>
 		<b-button @click="DrawData">DrawData</b-button>
-		<b-button @click="Delay(1000)">Test</b-button>
+		<b-button @click="DrawDataTest">Test</b-button>
 	
 		<div>{{factLen1}}</div>
 		
@@ -63,253 +63,256 @@
   //import '../../node_modules/echarts/map/js/world.js'
   import '../../node_modules/echarts/map/js/china.js' // 引入中国地图数据
   export default {
-    name: "echarts",
-    props: ["userJson"],
-    data() {
-      return {
-		
-		mapData:[
-    {name: '海门', value: 9},
-    {name: '鄂尔多斯', value: 12},
-    {name: '招远', value: 12},
-    {name: '舟山', value: 12},
-    {name: '齐齐哈尔', value: 14},
-    {name: '盐城', value: 15},
-    {name: '赤峰', value: 16},
-    {name: '青岛', value: 18},
-    {name: '乳山', value: 18},
-    {name: '金昌', value: 19},
-    {name: '泉州', value: 21},
-    {name: '莱西', value: 21},
-    {name: '日照', value: 21},
-    {name: '胶南', value: 22},
-    {name: '南通', value: 23},
-    {name: '拉萨', value: 24},
-    {name: '云浮', value: 24},
-    {name: '梅州', value: 25},
-    {name: '文登', value: 25},
-    {name: '上海', value: 25},
-    {name: '攀枝花', value: 25},
-    {name: '威海', value: 25},
-    {name: '承德', value: 25},
-    {name: '厦门', value: 26},
-    {name: '汕尾', value: 26},
-    {name: '潮州', value: 26},
-    {name: '丹东', value: 27},
-    {name: '太仓', value: 27},
-    {name: '曲靖', value: 27},
-    {name: '烟台', value: 28},
-    {name: '福州', value: 29},
-    {name: '瓦房店', value: 30},
-    {name: '即墨', value: 30},
-    {name: '抚顺', value: 31},
-    {name: '玉溪', value: 31},
-    {name: '张家口', value: 31},
-    {name: '阳泉', value: 31},
-    {name: '莱州', value: 32},
-    {name: '湖州', value: 32},
-    {name: '汕头', value: 32},
-    {name: '昆山', value: 33},
-    {name: '宁波', value: 33},
-    {name: '湛江', value: 33},
-    {name: '揭阳', value: 34},
-    {name: '荣成', value: 34},
-    {name: '连云港', value: 35},
-    {name: '葫芦岛', value: 35},
-    {name: '常熟', value: 36},
-    {name: '东莞', value: 36},
-    {name: '河源', value: 36},
-    {name: '淮安', value: 36},
-    {name: '泰州', value: 36},
-    {name: '南宁', value: 37},
-    {name: '营口', value: 37},
-    {name: '惠州', value: 37},
-    {name: '江阴', value: 37},
-    {name: '蓬莱', value: 37},
-    {name: '韶关', value: 38},
-    {name: '嘉峪关', value: 38},
-    {name: '广州', value: 38},
-    {name: '延安', value: 38},
-    {name: '太原', value: 39},
-    {name: '清远', value: 39},
-    {name: '中山', value: 39},
-    {name: '昆明', value: 39},
-    {name: '寿光', value: 40},
-    {name: '盘锦', value: 40},
-    {name: '长治', value: 41},
-    {name: '深圳', value: 41},
-    {name: '珠海', value: 42},
-    {name: '宿迁', value: 43},
-    {name: '咸阳', value: 43},
-    {name: '铜川', value: 44},
-    {name: '平度', value: 44},
-    {name: '佛山', value: 44},
-    {name: '海口', value: 44},
-    {name: '江门', value: 45},
-    {name: '章丘', value: 45},
-    {name: '肇庆', value: 46},
-    {name: '大连', value: 47},
-    {name: '临汾', value: 47},
-    {name: '吴江', value: 47},
-    {name: '石嘴山', value: 49},
-    {name: '沈阳', value: 50},
-    {name: '苏州', value: 50},
-    {name: '茂名', value: 50},
-    {name: '嘉兴', value: 51},
-    {name: '长春', value: 51},
-    {name: '胶州', value: 52},
-    {name: '银川', value: 52},
-    {name: '张家港', value: 52},
-    {name: '三门峡', value: 53},
-    {name: '锦州', value: 54},
-    {name: '南昌', value: 54},
-    {name: '柳州', value: 54},
-    {name: '三亚', value: 54},
-    {name: '自贡', value: 56},
-    {name: '吉林', value: 56},
-    {name: '阳江', value: 57},
-    {name: '泸州', value: 57},
-    {name: '西宁', value: 57},
-    {name: '宜宾', value: 58},
-    {name: '呼和浩特', value: 58},
-    {name: '成都', value: 58},
-    {name: '大同', value: 58},
-    {name: '镇江', value: 59},
-    {name: '桂林', value: 59},
-    {name: '张家界', value: 59},
-    {name: '宜兴', value: 59},
-    {name: '北海', value: 60},
-    {name: '西安', value: 61},
-    {name: '金坛', value: 62},
-    {name: '东营', value: 62},
-    {name: '牡丹江', value: 63},
-    {name: '遵义', value: 63},
-    {name: '绍兴', value: 63},
-    {name: '扬州', value: 64},
-    {name: '常州', value: 64},
-    {name: '潍坊', value: 65},
-    {name: '重庆', value: 66},
-    {name: '台州', value: 67},
-    {name: '南京', value: 67},
-    {name: '滨州', value: 70},
-    {name: '贵阳', value: 71},
-    {name: '无锡', value: 71},
-    {name: '本溪', value: 71},
-    {name: '克拉玛依', value: 72},
-    {name: '渭南', value: 72},
-    {name: '马鞍山', value: 72},
-    {name: '宝鸡', value: 72},
-    {name: '焦作', value: 75},
-    {name: '句容', value: 75}],
-		
-		geoCoordMap:{
-    '海门':[121.15,31.89],
-    '鄂尔多斯':[109.781327,39.608266],
-    '招远':[120.38,37.35],
-    '舟山':[122.207216,29.985295],
-    '齐齐哈尔':[123.97,47.33],
-    '盐城':[120.13,33.38],
-    '赤峰':[118.87,42.28],
-    '青岛':[120.33,36.07],
-    '乳山':[121.52,36.89],
-    '金昌':[102.188043,38.520089],
-    '泉州':[118.58,24.93],
-    '莱西':[120.53,36.86],
-    '日照':[119.46,35.42],
-    '胶南':[119.97,35.88],
-    '南通':[121.05,32.08],
-    '拉萨':[91.11,29.97],
-    '云浮':[112.02,22.93],
-    '梅州':[116.1,24.55],
-    '文登':[122.05,37.2],
-    '上海':[121.48,31.22],
-    '攀枝花':[101.718637,26.582347],
-    '威海':[122.1,37.5],
-    '承德':[117.93,40.97],
-    '厦门':[118.1,24.46],
-    '汕尾':[115.375279,22.786211],
-    '潮州':[116.63,23.68],
-    '丹东':[124.37,40.13],
-    '太仓':[121.1,31.45],
-    '曲靖':[103.79,25.51],
-    '烟台':[121.39,37.52],
-    '福州':[119.3,26.08],
-    '瓦房店':[121.979603,39.627114],
-    '即墨':[120.45,36.38],
-    '抚顺':[123.97,41.97],
-    '玉溪':[102.52,24.35],
-    '张家口':[114.87,40.82],
-    '阳泉':[113.57,37.85],
-    '莱州':[119.942327,37.177017],
-    '湖州':[120.1,30.86],
-    '汕头':[116.69,23.39],
-    '昆山':[120.95,31.39],
-    '宁波':[121.56,29.86],
-    '湛江':[110.359377,21.270708],
-    '揭阳':[116.35,23.55],
-    '荣成':[122.41,37.16],
-    '连云港':[119.16,34.59],
-    '葫芦岛':[120.836932,40.711052],
-    '常熟':[120.74,31.64],
-    '东莞':[113.75,23.04],
-    '河源':[114.68,23.73],
-    '淮安':[119.15,33.5],
-    '泰州':[119.9,32.49],
-    '南宁':[108.33,22.84],
-    '营口':[122.18,40.65],
-    '惠州':[114.4,23.09],
-    '江阴':[120.26,31.91],
-    '蓬莱':[120.75,37.8],
-    '韶关':[113.62,24.84],
-    '嘉峪关':[98.289152,39.77313],
-    '广州':[113.23,23.16],
-    '延安':[109.47,36.6],
-    '太原':[112.53,37.87],
-    '清远':[113.01,23.7],
-    '中山':[113.38,22.52],
-    '昆明':[102.73,25.04],
-    '寿光':[118.73,36.86],
-    '盘锦':[122.070714,41.119997],
-    '长治':[113.08,36.18],
-    '深圳':[114.07,22.62],
-    '珠海':[113.52,22.3],
-    '宿迁':[118.3,33.96],
-    '咸阳':[108.72,34.36],
-    '铜川':[109.11,35.09],
-    '平度':[119.97,36.77],
-    '佛山':[113.11,23.05],
-    '海口':[110.35,20.02],
-    '江门':[113.06,22.61],
-    '章丘':[117.53,36.72],
-    '肇庆':[112.44,23.05],
-    '大连':[121.62,38.92],
-    '临汾':[111.5,36.08],
-    '吴江':[120.63,31.16],
-    '石嘴山':[106.39,39.04],
-    '沈阳':[123.38,41.8],
-    '苏州':[120.62,31.32],
-    '茂名':[110.88,21.68],
-    '嘉兴':[120.76,30.77],
-    '长春':[125.35,43.88],
-    '胶州':[120.03336,36.264622],
-    '银川':[106.27,38.47],
-    '张家港':[120.555821,31.875428],
-    '三门峡':[111.19,34.76],
-    '锦州':[121.15,41.13],
-    '南昌':[115.89,28.68],
-    '柳州':[109.4,24.33],
-    '三亚':[109.511909,18.252847],
-    '自贡':[104.778442,29.33903],
-    '吉林':[126.57,43.87],
-    '阳江':[111.95,21.85],
-    '泸州':[105.39,28.91],
-    '西宁':[101.74,36.56],
-    '宜宾':[104.56,29.77]
- 		},
+					name: "echarts",
+					props: ["userJson"],
+					data() {
+						return {
+					
+					mapData:[
+					{name: '海门', value: 9},
+					{name: '鄂尔多斯', value: 12},
+					{name: '招远', value: 12},
+					{name: '舟山', value: 12},
+					{name: '齐齐哈尔', value: 14},
+					{name: '盐城', value: 15},
+					{name: '赤峰', value: 16},
+					{name: '青岛', value: 18},
+					{name: '乳山', value: 18},
+					{name: '金昌', value: 19},
+					{name: '泉州', value: 21},
+					{name: '莱西', value: 21},
+					{name: '日照', value: 21},
+					{name: '胶南', value: 22},
+					{name: '南通', value: 23},
+					{name: '拉萨', value: 24},
+					{name: '云浮', value: 24},
+					{name: '梅州', value: 25},
+					{name: '文登', value: 25},
+					{name: '上海', value: 25},
+					{name: '攀枝花', value: 25},
+					{name: '威海', value: 25},
+					{name: '承德', value: 25},
+					{name: '厦门', value: 26},
+					{name: '汕尾', value: 26},
+					{name: '潮州', value: 26},
+					{name: '丹东', value: 27},
+					{name: '太仓', value: 27},
+					{name: '曲靖', value: 27},
+					{name: '烟台', value: 28},
+					{name: '福州', value: 29},
+					{name: '瓦房店', value: 30},
+					{name: '即墨', value: 30},
+					{name: '抚顺', value: 31},
+					{name: '玉溪', value: 31},
+					{name: '张家口', value: 31},
+					{name: '阳泉', value: 31},
+					{name: '莱州', value: 32},
+					{name: '湖州', value: 32},
+					{name: '汕头', value: 32},
+					{name: '昆山', value: 33},
+					{name: '宁波', value: 33},
+					{name: '湛江', value: 33},
+					{name: '揭阳', value: 34},
+					{name: '荣成', value: 34},
+					{name: '连云港', value: 35},
+					{name: '葫芦岛', value: 35},
+					{name: '常熟', value: 36},
+					{name: '东莞', value: 36},
+					{name: '河源', value: 36},
+					{name: '淮安', value: 36},
+					{name: '泰州', value: 36},
+					{name: '南宁', value: 37},
+					{name: '营口', value: 37},
+					{name: '惠州', value: 37},
+					{name: '江阴', value: 37},
+					{name: '蓬莱', value: 37},
+					{name: '韶关', value: 38},
+					{name: '嘉峪关', value: 38},
+					{name: '广州', value: 38},
+					{name: '延安', value: 38},
+					{name: '太原', value: 39},
+					{name: '清远', value: 39},
+					{name: '中山', value: 39},
+					{name: '昆明', value: 39},
+					{name: '寿光', value: 40},
+					{name: '盘锦', value: 40},
+					{name: '长治', value: 41},
+					{name: '深圳', value: 41},
+					{name: '珠海', value: 42},
+					{name: '宿迁', value: 43},
+					{name: '咸阳', value: 43},
+					{name: '铜川', value: 44},
+					{name: '平度', value: 44},
+					{name: '佛山', value: 44},
+					{name: '海口', value: 44},
+					{name: '江门', value: 45},
+					{name: '章丘', value: 45},
+					{name: '肇庆', value: 46},
+					{name: '大连', value: 47},
+					{name: '临汾', value: 47},
+					{name: '吴江', value: 47},
+					{name: '石嘴山', value: 49},
+					{name: '沈阳', value: 50},
+					{name: '苏州', value: 50},
+					{name: '茂名', value: 50},
+					{name: '嘉兴', value: 51},
+					{name: '长春', value: 51},
+					{name: '胶州', value: 52},
+					{name: '银川', value: 52},
+					{name: '张家港', value: 52},
+					{name: '三门峡', value: 53},
+					{name: '锦州', value: 54},
+					{name: '南昌', value: 54},
+					{name: '柳州', value: 54},
+					{name: '三亚', value: 54},
+					{name: '自贡', value: 56},
+					{name: '吉林', value: 56},
+					{name: '阳江', value: 57},
+					{name: '泸州', value: 57},
+					{name: '西宁', value: 57},
+					{name: '宜宾', value: 58},
+					{name: '呼和浩特', value: 58},
+					{name: '成都', value: 58},
+					{name: '大同', value: 58},
+					{name: '镇江', value: 59},
+					{name: '桂林', value: 59},
+					{name: '张家界', value: 59},
+					{name: '宜兴', value: 59},
+					{name: '北海', value: 60},
+					{name: '西安', value: 61},
+					{name: '金坛', value: 62},
+					{name: '东营', value: 62},
+					{name: '牡丹江', value: 63},
+					{name: '遵义', value: 63},
+					{name: '绍兴', value: 63},
+					{name: '扬州', value: 64},
+					{name: '常州', value: 64},
+					{name: '潍坊', value: 65},
+					{name: '重庆', value: 66},
+					{name: '台州', value: 67},
+					{name: '南京', value: 67},
+					{name: '滨州', value: 70},
+					{name: '贵阳', value: 71},
+					{name: '无锡', value: 71},
+					{name: '本溪', value: 71},
+					{name: '克拉玛依', value: 72},
+					{name: '渭南', value: 72},
+					{name: '马鞍山', value: 72},
+					{name: '宝鸡', value: 72},
+					{name: '焦作', value: 75},
+					{name: '句容', value: 75}],
+					
+					geoCoordMap:{
+					'海门':[121.15,31.89],
+					'鄂尔多斯':[109.781327,39.608266],
+					'招远':[120.38,37.35],
+					'舟山':[122.207216,29.985295],
+					'齐齐哈尔':[123.97,47.33],
+					'盐城':[120.13,33.38],
+					'赤峰':[118.87,42.28],
+					'青岛':[120.33,36.07],
+					'乳山':[121.52,36.89],
+					'金昌':[102.188043,38.520089],
+					'泉州':[118.58,24.93],
+					'莱西':[120.53,36.86],
+					'日照':[119.46,35.42],
+					'胶南':[119.97,35.88],
+					'南通':[121.05,32.08],
+					'拉萨':[91.11,29.97],
+					'云浮':[112.02,22.93],
+					'梅州':[116.1,24.55],
+					'文登':[122.05,37.2],
+					'上海':[121.48,31.22],
+					'攀枝花':[101.718637,26.582347],
+					'威海':[122.1,37.5],
+					'承德':[117.93,40.97],
+					'厦门':[118.1,24.46],
+					'汕尾':[115.375279,22.786211],
+					'潮州':[116.63,23.68],
+					'丹东':[124.37,40.13],
+					'太仓':[121.1,31.45],
+					'曲靖':[103.79,25.51],
+					'烟台':[121.39,37.52],
+					'福州':[119.3,26.08],
+					'瓦房店':[121.979603,39.627114],
+					'即墨':[120.45,36.38],
+					'抚顺':[123.97,41.97],
+					'玉溪':[102.52,24.35],
+					'张家口':[114.87,40.82],
+					'阳泉':[113.57,37.85],
+					'莱州':[119.942327,37.177017],
+					'湖州':[120.1,30.86],
+					'汕头':[116.69,23.39],
+					'昆山':[120.95,31.39],
+					'宁波':[121.56,29.86],
+					'湛江':[110.359377,21.270708],
+					'揭阳':[116.35,23.55],
+					'荣成':[122.41,37.16],
+					'连云港':[119.16,34.59],
+					'葫芦岛':[120.836932,40.711052],
+					'常熟':[120.74,31.64],
+					'东莞':[113.75,23.04],
+					'河源':[114.68,23.73],
+					'淮安':[119.15,33.5],
+					'泰州':[119.9,32.49],
+					'南宁':[108.33,22.84],
+					'营口':[122.18,40.65],
+					'惠州':[114.4,23.09],
+					'江阴':[120.26,31.91],
+					'蓬莱':[120.75,37.8],
+					'韶关':[113.62,24.84],
+					'嘉峪关':[98.289152,39.77313],
+					'广州':[113.23,23.16],
+					'延安':[109.47,36.6],
+					'太原':[112.53,37.87],
+					'清远':[113.01,23.7],
+					'中山':[113.38,22.52],
+					'昆明':[102.73,25.04],
+					'寿光':[118.73,36.86],
+					'盘锦':[122.070714,41.119997],
+					'长治':[113.08,36.18],
+					'深圳':[114.07,22.62],
+					'珠海':[113.52,22.3],
+					'宿迁':[118.3,33.96],
+					'咸阳':[108.72,34.36],
+					'铜川':[109.11,35.09],
+					'平度':[119.97,36.77],
+					'佛山':[113.11,23.05],
+					'海口':[110.35,20.02],
+					'江门':[113.06,22.61],
+					'章丘':[117.53,36.72],
+					'肇庆':[112.44,23.05],
+					'大连':[121.62,38.92],
+					'临汾':[111.5,36.08],
+					'吴江':[120.63,31.16],
+					'石嘴山':[106.39,39.04],
+					'沈阳':[123.38,41.8],
+					'苏州':[120.62,31.32],
+					'茂名':[110.88,21.68],
+					'嘉兴':[120.76,30.77],
+					'长春':[125.35,43.88],
+					'胶州':[120.03336,36.264622],
+					'银川':[106.27,38.47],
+					'张家港':[120.555821,31.875428],
+					'三门峡':[111.19,34.76],
+					'锦州':[121.15,41.13],
+					'南昌':[115.89,28.68],
+					'柳州':[109.4,24.33],
+					'三亚':[109.511909,18.252847],
+					'自贡':[104.778442,29.33903],
+					'吉林':[126.57,43.87],
+					'阳江':[111.95,21.85],
+					'泸州':[105.39,28.91],
+					'西宁':[101.74,36.56],
+					'宜宾':[104.56,29.77]
+					},
         chart: null,
 				myChart:null,
 				timer: null,
 				factList1:[],
+				MakeDataInternal:4*1000,
+				MakeDataLen:47,
+				DefaultDelay:400,
 			
 	    };
     },
@@ -339,13 +342,12 @@
 				从微点获取数据
 			*/
 			makeData(){
-				 for (var i = 0; i < 300; i++) {
+				 for (var i = 0; i < this.MakeDataLen; i++) {
 						let i=Math.floor(Math.random()*100+1)
 						let areaName=this.mapData[i].name
 						let areaVale=this.mapData[i].value
 						let lng=this.geoCoordMap[areaName][0]
 						let lat=this.geoCoordMap[areaName][1]
-						console.log("======>",areaName,areaVale,this.geoCoordMap[areaName][0],this.geoCoordMap[areaName][1])
 						if (i%3==0){
 							this.factList1.push([0,lng,lat,areaVale])
 						}
@@ -356,28 +358,66 @@
 							this.factList1.push([2,lng,lat,areaVale])
 						}
 				 }
-				
 			},
 		
 			Sleep(ms) {
 					return new Promise(resolve => setTimeout(resolve, ms))
 			},
+			
+			DrawDataTest() {
+				let tLen=this.factList1.length
+				let perCnt=parseInt(tLen*this.DefaultDelay/this.MakeDataInternal)
+				let modTime=(tLen*this.DefaultDelay)%this.MakeDataInternal
+				let modCnt
+				if (modTime>0){
+					modCnt=modTime/this.DefaultDelay
+				}
+				for (var i = 0; i <tLen;i++) {
+						console.log("====>",i)
+						i=i+1
+				}
+				console.log("====>",modCnt,perCnt)
+				
+				
+			},
+			
 			/*	
 				在ECHART画出数据
 				没2分钟取数一次 时间： 2*60*1000MS
 				DELAY= 60*1000   /300
 			*/
 			async DrawData() {
-				
 				let tLen=this.factList1.length
-				
-				for (var i = 0; i < 300; i++) {
+				var perCnt=parseInt(tLen*this.DefaultDelay/this.MakeDataInternal)
+				var timesCnt=parseInt(this.MakeDataInternal/this.DefaultDelay)
+				var modTime=(tLen*this.DefaultDelay)%this.MakeDataInternal
+				var modCnt=0
+				if (modTime>0){
+					 modCnt=modTime/this.DefaultDelay
+				}
+				console.log("tLen======>",tLen)
+				console.log("perCnt======>",perCnt)
+				console.log("modCnt======>",modCnt)
+				console.log("timesCnt======>",timesCnt)
+	
+				var totalCnt=0
+				for (var i = 0; i <tLen;) {
 					var chart=this.myChart;
 					var option = this.myChart.getOption();
-					let e=this.factList1.pop()
-					option.series[e[0]].data =[{name:"上海", value:[e[1],e[2],e[3]]}]
+					for (var j=0;j<perCnt;j++){
+							let e=this.factList1.pop()
+							option.series[e[0]].data =[{name:"上海", value:[e[1],e[2],e[3]]}]
+					}
+					i=i+perCnt
+					if (modCnt>0){
+ 						let e=this.factList1.pop()
+ 						option.series[e[0]].data =[{name:"上海", value:[e[1],e[2],e[3]]}],
+ 						modCnt=modCnt-1
+						i=i+1
+					}
+					console.log("index======>",i,totalCnt++)
 					this.myChart.setOption(option);  
-					await	this.Sleep(60*1000/tLen)
+					await	this.Sleep(this.DefaultDelay)
 					this.ClearData()
 				}
 			},
@@ -505,7 +545,7 @@
 								}
 						},
 			    series: [{
-							symbolSize: 10,
+							symbolSize: 15,
 				      type: 'effectScatter',
               coordinateSystem: 'geo' ,// 对应上方配置
               name: '血压指标', // 浮动框的标题
@@ -528,7 +568,7 @@
 										},
 							},
 						},{
-							symbolSize: 10,
+							symbolSize: 15,
 				      type: 'effectScatter',
               coordinateSystem: 'geo' ,// 对应上方配置
               name: '脉搏次数', // 浮动框的标题
@@ -551,7 +591,7 @@
 										},
 							},
 						},{
-							symbolSize: 10,
+							symbolSize: 15,
 				      type: 'effectScatter',
               coordinateSystem: 'geo' ,// 对应上方配置
               name: '血小板指数', // 浮动框的标题
